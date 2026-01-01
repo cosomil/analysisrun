6f907b4c0f032346fbfea23724c4cbf6c73154e8
daichitakahashi <t.daichi.snd@gmail.com>
2026-01-01 23:13:50 +0900

    feat(analysisrun): interactiveモジュールのブラッシュアップ
    - NamedTuple対応の強化
    - ネストしたBaseModel対応
    - 仮想ファイル（存在するファイルパス/ファイルデータ)を表すカスタムインプットを実装

--- src/analysisrun/interactive.py
+++ src/analysisrun/interactive.py
@@ -1,12 +1,24 @@
-from typing import TypeVar, Type, Any, get_origin, Optional
 import inspect
+from dataclasses import is_dataclass
+from io import BytesIO
+from os import getcwd
 from pathlib import Path
+from typing import Annotated, Any, Optional, Type, TypeGuard, TypeVar, get_origin
 
-from pydantic import BaseModel, ValidationError, GetCoreSchemaHandler
+from pydantic import BaseModel, Field, ValidationError
 from pydantic_core import PydanticUndefined, core_schema
+from typing_extensions import deprecated
+
+T = TypeVar("T")
 
 
-T = TypeVar("T", bound=BaseModel)
+def _get_field_descriptions(typ) -> str | None:
+    if type(typ) is type(Annotated[None, None]):  # 'typing._AnnotatedAlias'クラス
+        for _, x in enumerate(typ.__metadata__):  # type: ignore
+            return _get_field_descriptions(x)
+    if type(typ) is type(Field()):
+        return typ.description  # type: ignore
+    return None
 
 
 def _prompt_for_value(
@@ -52,9 +64,21 @@ def _prompt_for_value(
     ):
         print(prompt)
         named_tuple_values = {}
+        field_defaults = field_type._field_defaults or dict()  # type: ignore
+        assert isinstance(field_defaults, dict)
 
         for sub_field in field_type._fields:  # type: ignore
-            sub_value = input(f"  - {sub_field}: ")
+            default = field_defaults.get(sub_field)
+            description = (
+                _get_field_descriptions(default) if default is not None else None
+            )
+            print("prompt")
+            prompt = (
+                f"  - {description} ({sub_field}): "
+                if description
+                else f"  - {sub_field}: "
+            )
+            sub_value = input(prompt)
             # 入力がない場合のフラグ
             if not sub_value and has_default:
                 return default_value
@@ -98,7 +122,11 @@ def _prompt_for_value(
 
         return tuple(tuple_values)
 
-    # その他の型の場合
+    # Pydanticモデルの場合（ネストした入力）
+    elif _is_pydantic_model(field_type):
+        return _scan_object(field_type, field_name)
+
+    # その他の型の場合（custom_inputを含む）
     else:
         value = input(prompt)
 
@@ -109,28 +137,26 @@ def _prompt_for_value(
         return value
 
 
-def scan_model_input(model_class: Type[T]) -> T:
-    """
-    モデルのフィールドをインタラクティブに入力し、モデルのインスタンスを返します。
+def _scan_object(model_class: Type[T], parent: Optional[str]) -> T:
+    assert issubclass(model_class, BaseModel), (
+        "model_class must be a Pydantic BaseModel"
+    )
 
-    note: フィールドがさらにBaseModelを継承している場合の処理は未実装です。
-
-    Parameters
-    ----------
-    model_class
-        Pydanticモデルクラス
-    """
     # 有効な入力値を保存する辞書
     valid_inputs = {}
     field_values = {}
 
+    # モデルのフィールドを取得する
+    # NoneTypeのフィールドについては、Noneで初期化しておく
+    fields = model_class.model_fields
+    for field_name, field_type in fields.items():
+        if field_type.annotation is type(None):
+            valid_inputs[field_name] = None
+
     while True:
         try:
-            # モデルのフィールドを取得
-            fields = model_class.__annotations__
-            field_values = {}
-
             # 前回有効だった入力を引き継ぎ
+            field_values = {}
             field_values.update(valid_inputs)
 
             # エラーフィールドのセット（初回は全フィールドを入力対象とする）
@@ -154,8 +180,10 @@ def scan_model_input(model_class: Type[T]) -> T:
                 )
 
                 # 値の入力
+                assert field_type.annotation is not None
+                display_field_name = f"{parent}.{field_name}" if parent else field_name
                 field_values[field_name] = _prompt_for_value(
-                    field_type, field_name, description, field_info
+                    field_type.annotation, display_field_name, description, field_info
                 )
 
             # モデルのインスタンスを作成してみる
@@ -169,14 +197,21 @@ def scan_model_input(model_class: Type[T]) -> T:
 
             if isinstance(e, ValidationError):
                 # エラーメッセージをフィールドごとにグループ化
-                field_errors = {}
+                field_errors: dict[str, list[str]] = {}
                 for error in e.errors():
                     if error["loc"]:
-                        field_name = error["loc"][0]
+                        field_name: str = error["loc"][0]  # type: ignore
                         error_fields.add(field_name)
-                        if field_name not in field_errors:
-                            field_errors[field_name] = []
-                        field_errors[field_name].append(error["msg"])
+
+                        index = error["loc"][1] if len(error["loc"]) > 1 else None
+                        display_field_name = (
+                            f"{field_name}[{index}]"
+                            if index is not None
+                            else field_name
+                        )
+                        if display_field_name not in field_errors:
+                            field_errors[display_field_name] = []
+                        field_errors[display_field_name].append(error["msg"])
 
                 # エラー内容を表示
                 for field_name, errors in field_errors.items():
@@ -185,7 +220,10 @@ def scan_model_input(model_class: Type[T]) -> T:
                         print(f"    - {error_msg}")
 
                 # エラーがないフィールドの値を保存
-                for field_name, value in field_values.items():
+                for (
+                    field_name,
+                    value,
+                ) in field_values.items():
                     if field_name not in error_fields:
                         valid_inputs[field_name] = value
 
@@ -199,6 +237,42 @@ def scan_model_input(model_class: Type[T]) -> T:
             print()
 
 
+def scan_model_input(model_class: Type[T]) -> T:
+    """
+    モデルのフィールドをインタラクティブに入力し、モデルのインスタンスを返します。
+    標準入力がリダイレクトされている場合はtar形式としてデータを読み込み、ファイル名／データを名前／データに変換します。
+
+    フィールドがBaseModelを実装している場合には再帰的に処理します。
+    BaseModelの配列のようなケースには未対応。
+
+    Parameters
+    ----------
+    model_class
+        Pydanticモデルクラス
+    """
+    return _scan_object(model_class, None)
+
+
+def custom_input():
+    def wrapper(source_type):
+        setattr(source_type, "__analysisrun_custom_input__", True)
+        return source_type
+
+    return wrapper
+
+
+def _is_custom_input(v) -> bool:
+    return hasattr(v, "__analysisrun_custom_input__")
+
+
+def _is_pydantic_model(v) -> TypeGuard[Type[BaseModel]]:
+    return not _is_custom_input(v) and (
+        is_dataclass(v) or hasattr(v, "__get_pydantic_core_schema__")
+    )
+
+
+@custom_input()
+@deprecated("削除予定。VirtualFileを使用してください。")
 class FilePath(str):
     """
     ファイルパスを表す文字列型。バリデーションの際にファイルの存在を確認します。
@@ -206,7 +280,7 @@ class FilePath(str):
     """
 
     @classmethod
-    def __get_pydantic_core_schema__(cls, source_type, handler: GetCoreSchemaHandler):
+    def __get_pydantic_core_schema__(cls, source_type, handler):
         def validate(v):
             if not isinstance(v, str):
                 return v
@@ -222,6 +296,8 @@ class FilePath(str):
         return core_schema.no_info_plain_validator_function(validate)
 
 
+@custom_input()
+@deprecated("削除予定")
 class DirectoryPath(str):
     """
     ディレクトリパスを表す文字列型。バリデーションの際にディレクトリの存在を確認します。
@@ -229,7 +305,7 @@ class DirectoryPath(str):
     """
 
     @classmethod
-    def __get_pydantic_core_schema__(cls, source_type, handler: GetCoreSchemaHandler):
+    def __get_pydantic_core_schema__(cls, source_type, handler):
         def validate(v):
             if not isinstance(v, str):
                 return v
@@ -243,5 +319,56 @@ class DirectoryPath(str):
                     f"ディレクトリ（フォルダー）のパスを指定してください: '{v}'"
                 )
             return v
+
+        return core_schema.no_info_plain_validator_function(validate)
+
+
+@custom_input()
+class VirtualFile(Path):
+    """
+    仮想ファイルを扱う型。Pathオブジェクトまたはio.BytesIOオブジェクトを受け入れます。
+    Pathオブジェクトのようにふるまうほか、io.BytesIOオブジェクトが与えられた場合にはファイルのように振る舞います。
+    pandasのread_csvで読み込みができることを確認しています。
+
+    Pathあるいは文字列が与えられた場合:
+    - ファイルの存在を確認し、存在しなければエラーとなります。
+
+    io.BytesIOが与えられた場合:
+    - 作業ディレクトリに存在する仮想ファイルのように振る舞います。
+    - ファイル名は仮想的に"virtual-file"とします。
+    """
+
+    def __init__(self, v: Path | BytesIO):
+        if isinstance(v, Path):
+            super().__init__(v)
+        elif isinstance(v, BytesIO):
+            super().__init__(Path(getcwd()) / "virtual-file")
+
+            # io.BytesIOが与えられた場合のみ、FileLikeオブジェクトとして振る舞うためのメソッドを追加
+            def read(self: VirtualFile | None = None) -> bytes:
+                return v.read()
+
+            def iter(self: VirtualFile | None = None):
+                raise NotImplementedError()
+
+            self.read = read
+            self.__iter__ = iter
+
+    @classmethod
+    def __get_pydantic_core_schema__(cls, source_type, handler):
+        def validate(v):
+            if isinstance(v, (str, Path)):
+                if isinstance(v, str):
+                    if v.startswith("'") and v.endswith("'"):
+                        v = v[1:-1]
+                    v = Path(v)
+                if not v.exists():
+                    raise ValueError(f"ファイルが存在しません: '{v}'")
+                if not v.is_file():
+                    raise ValueError(f"ファイルのパスを指定してください: '{v}'")
+                return cls(v)
+            elif isinstance(v, BytesIO):
+                return cls(v)  # type: ignore
+            raise TypeError(f"Unsupported type: {type(v)}")
 
         return core_schema.no_info_plain_validator_function(validate)
