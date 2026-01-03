c48312baf2e7b6e7f452dd0785d70318433541e3
daichitakahashi <t.daichi.snd@gmail.com>
2026-01-03 14:43:17 +0900

    feat(analysisrun): エラー出力用関数を実装

---
+++ src/analysisrun/pipeable_io.py
@@ -0,0 +1,76 @@
+import sys
+import traceback
+from enum import Enum
+from typing import IO, NoReturn, Optional
+
+from pydantic import BaseModel, Field
+
+from analysisrun.__env import get_interactivity
+from analysisrun.helper import cowsay
+from analysisrun.tar import create_tar_from_dict
+
+
+class ExitCodes(Enum):
+    """
+    解析の異常終了時の終了コード
+    """
+
+    PROCESSING_ERROR = 1
+    INVALID_USAGE = 2
+
+
+class ErrorResult(BaseModel):
+    """
+    標準出力を通じて返されるエラー情報
+    """
+
+    error: str = Field(description="エラーメッセージ")
+
+
+def _print(msg: str | bytes, out: IO[bytes]) -> None:
+    if isinstance(msg, str):
+        out.write((msg + "\n").encode(encoding="utf-8"))
+    else:
+        out.write(msg)
+
+
+def exit_with_error(
+    code: ExitCodes,
+    message: str,
+    stdout: IO[bytes],
+    stderr: IO[bytes],
+    exception: Optional[Exception] = None,
+) -> NoReturn:
+    """
+    解析の異常終了を行う。
+    標準エラー出力にエラー情報を出力するほか、スタックトレースやユーザーフレンドリーな
+    エラーメッセージを標準エラー出力に出力する。
+
+    Parameters
+    ----------
+    code
+        終了コード
+    message
+        エラーメッセージ
+    stdout
+        標準出力ストリーム
+    stderr
+        標準エラー出力ストリーム
+    exception
+        発生した例外オブジェクト
+    """
+
+    if exception is not None:
+        print(exception, file=sys.stderr)
+        details = traceback.format_exception(exception)
+        _print("".join(details), stderr)
+        stderr.flush()
+
+    interactivity = get_interactivity()
+    if interactivity is None:
+        tar_data = create_tar_from_dict(ErrorResult(error=message).model_dump())
+        _print(tar_data.getvalue(), stdout)
+    else:
+        _print(cowsay(message), stdout)
+    stdout.flush()
+    sys.exit(code.value)
